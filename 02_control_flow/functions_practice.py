77f5ffe4db12e60c0f54f25ee711be04fadfecca
Rishabh Singh <rishabhhh05@gmail.com>
2025-12-27 22:31:08 +0530

    Add first Python function for basic logic practice

---
+++ 02_control_flow/functions_practice.py
@@ -0,0 +1,12 @@
+def sum(x,y):
+  return x+y
+
+def checkEvenOdd(x):
+  if(x%2 == 0):
+    print(f"{x} is an Even number.")
+  else:
+    print(f"{x} is an Odd number.")
+
+print(sum(5,4))
+checkEvenOdd(5)
+checkEvenOdd(4)
