084e6089107a9cc13be8d662b48722ee85a8cdf1
Rishabh Singh <rishabhhh05@gmail.com>
2025-12-27 23:42:13 +0530

    Add function using loop for repeated logic
    Added functions to find even numbers and calculate sum of series.

--- 02_control_flow/functions_practice.py
+++ 02_control_flow/functions_practice.py
@@ -10,3 +10,23 @@ def checkEvenOdd(x):
 print(sum(5,4))
 checkEvenOdd(5)
 checkEvenOdd(4)
+
+def findEven(n):
+  for i in range(n+1):
+    if(i%2 == 0):
+      print(i, end = " ")
+
+findEven(10)
+
+def sum_series(n):
+  ans = 0
+  for i in range(1,n+1):
+    ans += i
+  return ans
+
+print(sum_series(10))
+
+  
+
+  
+    
